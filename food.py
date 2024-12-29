a42ee8c8e3a1fbd0fa9bdb6c6c053852f91c623e
Aaron Ramos <ramosab@hawaii.edu>
2024-12-29 00:02:35 -1000

    Initial commit

---
+++ food.py
@@ -0,0 +1,12 @@
+class meal:
+    def __init__ (self, desc, cal, protein):
+        self.desc = desc
+        self.cal = cal
+        self.protein = protein
+
+class snack:
+    def __init__ (self, name, servings, cal, protein):
+        self.name = name
+        self.servings = servings
+        self.cal = cal
+        self.protein = protein
---
+++ weight-tracker.py
@@ -0,0 +1,29 @@
+def main():
+    getCal()
+    saveCal()
+    showCal()
+
+def getCal():
+    print("getCal() running successfully")
+
+""" mealTypes = [Breakfast, Lunch, Dinner, Snack]
+
+    while True: 
+        print(“What type of food are you logging?”)
+        for i, mealType in enumerate(mealTypes):
+        print(f”{i+1}. {mealType}”)
+        break
+
+        possVal = f”[ 1 - {len(mealTypes)} ]”
+        chosenType = int(input(f“What kind of food are you logging? Enter a value {possVal}:”)) - 1
+"""
+
+
+def saveCal():
+    print("saveCal() running successfully")
+
+def showCal():
+    print("showCal() running successfully")
+
+if __name__  == "__main__":
+    main()
