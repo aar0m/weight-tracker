72f9c211fe7b2d806dccc40255e97adff90995b6
Aaron Ramos <122333150+aar0m@users.noreply.github.com>
2024-12-31 18:15:34 -1000

    weight function + optimize prints

--- food.py
+++ food.py
@@ -1,15 +1,17 @@
 class Meal:
-    def __init__ (self, desc, cal, protein):
+    def __init__ (self, date, desc, cal, protein):
+        self.date = date
         self.type = 'Meal'
         self.desc = desc
         self.cal = cal
         self.protein = protein
 
     def __repr__(self):
-        return f"[{self.type}: {self.desc}, {self.cal:.0f} calories, {self.protein:.0f}g protein]"
+        return f"[{self.date}] {self.type}:{self.desc}, {self.cal:.0f} calories, {self.protein:.0f}g protein"
 
 class Snack:
-    def __init__ (self, name, servings, servCal, servProtein):
+    def __init__ (self, date, name, servings, servCal, servProtein):
+        self.date = date
         self.type = 'Snack'
         self.name = name
         self.servings = servings
@@ -19,4 +21,12 @@ class Snack:
         self.protein = self.servings * self.servPrt
     
     def __repr__(self):
-        return f"[{self.type}: {self.name}, {self.servings:.0f} servings, {self.cal:.0f} calories, {self.protein:.0f}g protein]"
+        return f"[{self.date}] {self.type}:{self.name}, {self.servings:.0f} servings, {self.cal:.0f} calories, {self.protein:.0f}g protein"
+    
+class Weight:
+    def __init__ (self, date, weight):
+        self.date = date
+        self.weight = weight
+    
+    def __repr__(self):
+        return f"[{self.date}] {self.weight}lbs"
--- weight-tracker.py
+++ weight-tracker.py
@@ -9,14 +9,7 @@ import datetime
 size = os.get_terminal_size()
 
 def prLine():
-    print("\n")
-    print("=" * size.columns)
-    print("\n")
-
-def prLineThin():
-    print("\n")
-    print("-" * size.columns)
-    print("\n")
+    print("\n" + "=" * size.columns + "\n")
 
 def prProgHead():
     size = os.get_terminal_size()
@@ -29,25 +22,20 @@ def prProgHead():
     print("Based on 'Expense Tracking App' Project by pixegami".center(size.columns))
     prLine()
 
-def prCalHead():
+def prMethodHead(text):
     prLine()
-    print("Calorie Logging Process Initiated".center(size.columns))
+    print(f"{text}".center(size.columns))
     prLine()
 
-def prSummHead():
-    prLine()
-    print(f"Summarizing Calories Consumed".center(size.columns))
-    prLineThin()
-
-def prDetailHead():
-    prLine()
-    print(f"Providing Caloric Breakdown and Protein Consumed".center(size.columns))
-    prLineThin()
+def prErrorMes(text):
+    print("\n" + ("x" * int(size.columns/2)).center(size.columns) + "\n")
+    print(f"{text}".center(size.columns))
+    print("\n" + ("x" * int(size.columns/2)).center(size.columns) + "\n")
 
 # Logic functions ////////////
 
 def getCal():
-    prCalHead()
+    prMethodHead("Calorie Logging Process Initiated")
     mealTypes = ["Breakfast", "Lunch", "Dinner", "Snack"]
 
     while True: 
@@ -57,25 +45,23 @@ def getCal():
         break
 
     possVal = f"[1 - {len(mealTypes)}]"
-    chosenType = int(input(f"\nEnter a value from {possVal}: ")) - 1
-
+    
     try:
-        print(f"\nYou are logging a {mealTypes[chosenType]}.")
-    except:
-        print(f"ERROR: Please enter a value from {possVal}.")
+        chosenType = int(input(f"\nEnter a value from {possVal}: ")) - 1
+    except ValueError:
+        prErrorMes(f"ERROR: Please enter a value from {possVal}.")
         exit
+
+    print(f"\nYou are logging a {mealTypes[chosenType]}.")
 
     if chosenType in range(len(mealTypes) - 1): # For Meals
         mealDesc = html.escape(input("Describe your meal (what did you eat?):\n"))
         eatenCal = int(input("\nEnter the number of calories(kCal) eaten: "))
         eatenPrt = int(input("Enter the number of protein(g) in the meal: "))
 
-        loggedFood = Meal(desc=mealDesc, cal=eatenCal, protein=eatenPrt)
+        loggedFood = Meal(date=datetime.date.today(), desc=mealDesc, cal=eatenCal, protein=eatenPrt)
         
-        prLine()
-        print(f"Logging {loggedFood.cal} calories and {loggedFood.protein}g protein consumed for {mealTypes[chosenType]}.".center(size.columns))
-        print(f"You ate {loggedFood.desc}!".center(size.columns))
-        prLine()
+        prMethodHead(f"Logging {loggedFood.cal} calories and {loggedFood.protein}g protein consumed for {mealTypes[chosenType]}.\nYou ate {loggedFood.desc}!")
 
     elif chosenType + 1 == len(mealTypes): # For Snacks
         snackName = html.escape(input("What is the name of your snack?: "))
@@ -83,117 +69,145 @@ def getCal():
         servCals  = int(input("How many calories(kCal) are in each serving?: "))
         servPrt  = int(input("Enter the number of protein(g) in each serving: "))
 
-        loggedFood = Snack(name=snackName, servings=servSize, servCal=servCals, servProtein=servPrt)
+        loggedFood = Snack(date=datetime.date.today(), name=snackName, servings=servSize, servCal=servCals, servProtein=servPrt)
        
-        prLine()
-        print(f"Logging {loggedFood.cal:.0f} calories and {loggedFood.protein:.0f}g protein consumed for a {mealTypes[chosenType]}.".center(size.columns))
-        print(f"You ate {loggedFood.servings:.0f} servings of {loggedFood.name}!".center(size.columns))
-        prLine()
+        prMethodHead(f"Logging {loggedFood.cal:.0f} calories and {loggedFood.protein:.0f}g protein consumed for a {mealTypes[chosenType]}.\nYou ate {loggedFood.servings:.0f} servings of {loggedFood.name}!")
 
     return loggedFood
 
 def saveCal(food, cal_file_path):
-    print(f"Recorded {food} to {cal_file_path}!")
-    prLine()
+    prMethodHead(f"Recorded {food} to {cal_file_path}!")
     
     with open(cal_file_path, "a") as f:
         if isinstance(food, Meal):
-            f.write(f"M, {food.desc},{food.cal},{food.protein}\n")
+            f.write(f"{datetime.date.today()}, M, {food.desc},{food.cal},{food.protein}\n")
 
         elif isinstance(food, Snack):
-            f.write(f"S, {food.name},{food.servings},{food.cal},{food.protein}\n")
+            f.write(f"{datetime.date.today()}, S, {food.name},{food.servings},{food.cal},{food.protein}\n")
+    f.close()
 
-def readList(cal_file_path):
+def readCalList(cal_file_path):
     foodCal = []
 
     with open(cal_file_path, "r") as f:
         lines = f.readlines()
 
         for line in lines:
-            if line[0] == 'M':
-                calType, mealDesc, mealCal, mealPrt = line.strip().split(",")
-                line_cal = Meal(desc=mealDesc, cal=int(mealCal), protein=int(mealPrt))
+            if line[12] == 'M':
+                calDate, calType, mealDesc, mealCal, mealPrt = line.strip().split(",")
+                line_cal = Meal(date=calDate, desc=mealDesc, cal=int(mealCal), protein=int(mealPrt))
 
-            elif line[0] == 'S':
-                calType, snackName, snackServ, snackCal, snackPrt = line.strip().split(",")
+            elif line[12] == 'S':
+                calDate, calType, snackName, snackServ, snackCal, snackPrt = line.strip().split(",")
                 line_cal = Snack(
+                    date        = calDate,
                     name        = snackName, 
                     servings    = float(snackServ), 
                     servCal     = (float(snackCal) / float(snackServ)), 
                     servProtein = (float(snackPrt) / float(snackServ)))
             
             foodCal.append(line_cal)
+    f.close()
     
     return foodCal
 
 def summCal(cal_file_path):
-    prSummHead()
-    foodCal = readList(cal_file_path)
+    prMethodHead("Summarizing Calories Consumed")
+    foodCal = readCalList(cal_file_path)
 
-    print("Food Eaten".center(32, "-").center(size.columns))
     for food in foodCal:
         print(f"{food}".center(size.columns).ljust(size.columns))
     prLine()
 
-def summDetails(cal_file_path):
-    prDetailHead()
-    foodCal = readList(cal_file_path)
+def summCalDetails(cal_file_path):
+    prMethodHead("Providing Daily Intake of Calories and Protein")
+    foodCal = readCalList(cal_file_path)
 
-    calByType = {}
+    dailyCal = {}
     for food in foodCal:
-        key = food.type
-        if key in calByType:
-            calByType[key] += food.cal
+        key = food.date
+        if key in dailyCal:
+            dailyCal[key] = [dailyCal[key][0] + food.cal, dailyCal[key][1] + food.protein]
         else:
-            calByType[key] = food.cal
+            dailyCal[key] = [food.cal, food.protein]
 
-
-    prtByType = {}
-    for food in foodCal:
-        key = food.type
-        if key in prtByType:
-            prtByType[key] += food.protein
-        else:
-            prtByType[key] = food.protein
-
-    print("Calorie Breakdown:".center(32, "-").center(size.columns))
-    for typeCal, amount in calByType.items():
-        print(f"{typeCal}s: {amount} calories".center(size.columns))
-    prLineThin()
-
-    print("Protein Breakdown:".center(32, "-").center(size.columns))
-    for typeCal, amount in prtByType.items():
-        print(f"{typeCal}s: {amount}g protein".center(size.columns))
+    for day, food in dailyCal.items():
+        print(f"[{day}] {food[0]} calories, {food[1]}g protein".center(size.columns))
     prLine()
+
+def getWeight():
+    prMethodHead("Weight Logging Process Initiated")
+
+    try:
+        weight = float(input("Please enter your measured weight (lbs): "))
+
+    except ValueError:
+        prErrorMes("ERROR: Only enter the number of your measured weight (lbs).")
+        exit
+    
+    return weight
+
+def saveWeight(weight, weight_file_path):
+    prMethodHead("Recorded {weight}lbs to {weight_file_path}!")
+    
+    try:
+        with open(weight_file_path, "r+") as f:
+            lines = f.readlines()
+            # DEBUG: print(f"{lines[len(lines)-1].split(",")[0] == str(datetime.date.today())}")
+
+            if lines[-1].split(",")[0] == str(datetime.date.today()):
+                lines[-1] = f"{datetime.date.today()}, {weight}\n"
+                
+                with open(weight_file_path, "w") as f:
+                    f.writelines(lines)
+            else:
+                with open(weight_file_path, "a") as f:
+                    f.write(f"{datetime.date.today()}, {weight}\n")
+        f.close()
+
+    except FileNotFoundError:
+        with open(weight_file_path, "a") as f:
+            f.write(f"{datetime.date.today()}, {weight}\n")
+        f.close()
 
 def main():
     prProgHead()
     #TODO: Weight input using datetime
     #TODO: Average weight/week/month (good luck lol)
 
-    cal_file_path = "calorie.csv"
+    cal_file_path = f"calorie{datetime.date.today().strftime("%Y")}.csv"
+    weight_file_path = f"weight{datetime.date.today().strftime("%Y")}.csv"
     
-    options = ["Log Calories", "Show Food Log", "Calorie/Protein Breakdown", "Exit"]
+    options = ["Log Calories", "Log Weight", "Show Entire Food Log", "Show Daily Intake", "Exit"]
 
     while True: 
-        print("\nWelcome! What would you like to do today?")
+        print("Welcome! What would you like to do today?")
         for i, option in enumerate(options):
             print(f"{i+1}. {option}")
         
         possVal = f"[1 - {len(options)}]"
-        chosenOpt = int(input(f"\nEnter a value from {possVal}: ")) - 1
+        
+        try:
+            chosenOpt = int(input(f"\nEnter a value from {possVal}: ")) - 1
+        except ValueError:
+            chosenOpt = 99999
         
         match chosenOpt:
             case 0:
                 food = getCal()
                 saveCal(food, cal_file_path)
             case 1:
-                summCal(cal_file_path)
+                weight = getWeight()
+                saveWeight(weight, weight_file_path)
             case 2:
-                summDetails(cal_file_path)
+                summCal(cal_file_path)
             case 3:
+                summCalDetails(cal_file_path)
+            case 4:
                 print("Exiting Now".center(32, "-"), "\n")
                 break
+            case _:
+                prErrorMes(f"ERROR: Invalid input. Please enter a value from {possVal}.")
 
 if __name__  == "__main__":
     main()
